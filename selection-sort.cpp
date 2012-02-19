aac5ca748dfab8a8c003b7d1370b9bf2599f9cd6
Michael Parsons <mparsons1771@gmail.com>
2012-02-18 20:04:01 -0600

    Updated bubble and selection sort

--- selection-sort.cpp
+++ selection-sort.cpp
@@ -9,10 +9,22 @@
 #include "sort.h"
 #include <iostream>
 
-void
+void 
 SelectionSort::sort(int A[], int size)				// main entry point
 {
   /* Complete this function with the implementation of selection sort algorithm 
   Record number of comparisons in variable num_cmps of class Sort
   */
+	for (int k = 0; k < size-1; k++)
+	{
+		int index = k;
+		int tmp;
+		for (int i = k+1; i < size; i++)
+		{
+			if ((num_cmps++, A[i] < A[index])) index = i;
+		}
+		tmp = A[k]; // swap A[k] and A[index]
+		A[k] = A[index];
+		A[index] = tmp;
+	}
 }
